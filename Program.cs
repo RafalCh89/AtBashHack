cbb7370a45de54a0fb44e51d380e917ea88771a5
RafalCh89 <rafal.choma@hotmail.com>
2017-09-10 15:54:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+
+namespace Szyfr_AtBash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter letter:");
+            int letter = Console.Read();
+            Console.WriteLine("(ASCII) code : " + letter);
+            letter = 97 + (25 - letter + 97) % 26;
+            char letternew = (char)letter;
+            Console.WriteLine("Chacked: " + letternew);
+            Console.ReadKey();
+        }
+    }
+}
